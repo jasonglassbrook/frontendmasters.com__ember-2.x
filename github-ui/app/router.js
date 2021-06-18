ec9b7860df3d7c3669a54496e97b0aba767d66a4
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2021-06-18 16:29:47 -0400

    feat: add routes for org(s), repo(s), issues, contributors

--- github-ui/app/router.js
+++ github-ui/app/router.js
@@ -1,13 +1,22 @@
-import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import EmberRouter from "@ember/routing/router";
+import config from "./config/environment";
 
 const Router = EmberRouter.extend({
   location: config.locationType,
-  rootURL: config.rootURL
+  rootURL: config.rootURL,
 });
 
-Router.map(function() {
-  this.route('home');
+Router.map(function () {
+  this.route("home");
+  this.route("orgs");
+  this.route("org", { path: "org/:org" }, function () {
+    this.route("repos");
+    this.route("repo", { path: "repo/:repo" }, function () {
+      this.route("contributors");
+      this.route("issues");
+    });
+  });
+  this.route("not-found", {  path: "*path"  });
 });
 
 export default Router;
