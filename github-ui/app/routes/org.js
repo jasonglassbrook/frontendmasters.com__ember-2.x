de758f2244f5a0965ad96def251188bcbb6d4dc4
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2021-06-22 13:29:47 -0400

    feat: fetch and show org data from GitHub API

--- github-ui/app/routes/org.js
+++ github-ui/app/routes/org.js
@@ -1,3 +1,18 @@
 import Route from "@ember/routing/route";
+import api from "github-ui/helpers/github-api";
 
-export default Route.extend({});
+export default Route.extend({
+  model(params) {
+    const orgId = params.org;
+    const orgApiUri = api.org(orgId).index;
+
+    return $.get(orgApiUri).then((org) => {
+      // Backup GitHub's internal numeric ID.
+      org.github_id = org.id;
+      // Use my ID instead.
+      org.id = orgId;
+
+      return org;
+    });
+  },
+});
