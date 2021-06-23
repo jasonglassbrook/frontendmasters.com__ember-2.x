3d19bd3a5e10b9d9f36ccdc93ec3b84a4e6b6e1d
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2021-06-23 15:17:51 -0400

    feat: fetch and show repo data from GitHub API

--- github-ui/app/routes/org/repo.js
+++ github-ui/app/routes/org/repo.js
@@ -1,3 +1,20 @@
 import Route from "@ember/routing/route";
+import { get } from "@ember/object";
+import api from "github-ui/helpers/github-api";
 
-export default Route.extend({});
+export default Route.extend({
+  model(params) {
+    const orgId = get(this.modelFor("org"), "id");
+    const repoId = params.repo;
+    const repoApiUri = api.org(orgId).repo(repoId).index;
+
+    return $.get(repoApiUri).then((repo) => {
+      // Backup GitHub's internal numeric ID.
+      repo.github_id = repo.id;
+      // Use my ID instead.
+      repo.id = repoId;
+
+      return repo;
+    });
+  },
+});
