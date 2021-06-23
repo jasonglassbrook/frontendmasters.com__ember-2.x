e1957420e70f0094f7d8698a64f072987453834e
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2021-06-23 12:23:35 -0400

    feat: fetch and show org.repos data from GitHub API

--- github-ui/app/routes/org/repos.js
+++ github-ui/app/routes/org/repos.js
@@ -1,11 +1,22 @@
 import Route from "@ember/routing/route";
+import { get } from "@ember/object";
+import api from "github-ui/helpers/github-api";
 
 export default Route.extend({
   model() {
-    return [
-      { id: "repo-a", name: "Repo A" },
-      { id: "repo-b", name: "Repo B" },
-      { id: "repo-c", name: "Repo C" },
-    ];
+    const orgId = get(this.modelFor("org"), "id");
+    const orgReposApiUri = api.org(orgId).repos;
+
+    return $.get(orgReposApiUri).then((orgRepos) => {
+      // We gotta edit each repo's data...
+      return orgRepos.map((repo) => {
+        // Backup GitHub's internal numeric ID.
+        repo.github_id = repo.id;
+        // Use my ID instead.
+        repo.id = repo.name;
+
+        return repo;
+      });
+    });
   },
 });
