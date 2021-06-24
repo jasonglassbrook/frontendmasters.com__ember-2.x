982d77dad4e47786fa0b794942b880caf3dcd275
Jason Glassbrook <28660371+jasonglassbrook@users.noreply.github.com>
2021-06-24 16:34:20 -0400

    feat: fetch and show repo.issues data from GitHub API

--- github-ui/app/routes/org/repo/issues.js
+++ github-ui/app/routes/org/repo/issues.js
@@ -1,11 +1,24 @@
 import Route from "@ember/routing/route";
+import { get } from "@ember/object";
+
+import api from "github-ui/helpers/github-api";
+import { injectDatetimeRecord } from "github-ui/helpers/datetime";
 
 export default Route.extend({
   model() {
-    return [
-      { id: "issue-a", name: "Issue A" },
-      { id: "issue-b", name: "Issue B" },
-      { id: "issue-c", name: "Issue C" },
-    ];
+    const orgId = get(this.modelFor("org"), "id");
+    const repoId = get(this.modelFor("org.repo"), "id");
+    const repoIssuesApiUri = api.org(orgId).repo(repoId).issues;
+
+    return $.get(repoIssuesApiUri).then((repoIssues) => {
+      // We gotta edit each repo's data...
+      return repoIssues.map((issue) => {
+        // Process "created at" and "updated at" strings for display.
+        injectDatetimeRecord(issue, issue.created_at, "created_at__", "");
+        injectDatetimeRecord(issue, issue.updated_at, "updated_at__", "");
+
+        return issue;
+      });
+    });
   },
 });
